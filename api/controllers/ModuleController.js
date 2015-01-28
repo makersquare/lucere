e1272a313e4e0f12db9915872b403cc1bf7f98fb
Krish Rajagopalan <krish.andres@gmail.com>
2015-01-28 15:13:43 -0800

    Create module API with attributes and associations

---
+++ api/controllers/ModuleController.js
@@ -0,0 +1,11 @@
+/**
+ * ModuleController
+ *
+ * @description :: Server-side logic for managing modules
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
