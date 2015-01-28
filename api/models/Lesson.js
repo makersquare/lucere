2e1850cccdece2dcf39e713f4d193af1da5f26c8
Kenny Czadzeck <kennycgit@gmail.com>
2015-01-28 15:09:31 -0800

    add lesson model attributes.

--- api/models/Lesson.js
+++ api/models/Lesson.js
@@ -9,6 +9,16 @@ module.exports = {
 
   attributes: {
 
+    title: {
+      type: "string",
+      required: true
+    },
+
+    body: {
+      type: "string",
+      required: true
+    }
+
   }
 };
 
