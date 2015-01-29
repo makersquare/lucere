fe460e21246f0d3fae4d1a28af312e60fac103b7
Sean Gibat <sgibat@gmail.com>
2015-01-28 17:14:26 -0800

    set development and production migration variables

--- config/env/production.js
+++ config/env/production.js
@@ -12,6 +12,10 @@
 
 module.exports = {
 
+  models: {
+    migrate: 'safe'
+  }
+
   /***************************************************************************
    * Set the default database connection for models in the production        *
    * environment (see config/connections.js and config/models.js )           *
