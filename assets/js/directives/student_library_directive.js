a9900893274fd65ae10d0f8406f8a1dabf42e58d
Krish Rajagopalan <krish.andres@gmail.com>
2015-02-02 14:52:15 -0800

    Adds angular view for module and create sstudent and core library views

---
+++ assets/js/directives/student_library_directive.js
@@ -0,0 +1,11 @@
+app.directive("studentLibraryDirective", ["Library", "$route", function(Library, $route) {
+  return {
+    templateUrl: "/js/templates/student_library_template.html",
+    link: function(scope, attr, elem) {
+      scope.params = $route.current.params;
+      var id = scope.params.libraryId;
+      scope.library = Library.get({id: id});
+      
+    }
+  }
+}]);
