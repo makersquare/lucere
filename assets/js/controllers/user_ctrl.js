15f94a2784f1d658db2e16a14b7976011d4ef3bd
Tessa <tessakkelly@gmail.com>
2015-02-02 23:57:10 -0800

    add to user_profile_template

--- assets/js/controllers/user_ctrl.js
+++ assets/js/controllers/user_ctrl.js
@@ -7,4 +7,20 @@ app.controller("userCtrl", ["$scope", "$routeParams", "User", "AuthService", fun
     $scope.isOwn = userId === user.id;
   });
 
+  $scope.update = function() {
+    console.log("clicked")
+    angular.element("#user-profile").children("input").forEach(function(v) {
+      console.log(v.val());
+    });
+  }
+
+  $scope.editUser = function() {
+    var $elem = angular.element("#user-profile");
+    $elem.children().remove();
+    $elem.append("<input type='text' value='"+ $scope.user.name + "'><br>");
+    $elem.append("<input type='text' value='" + $scope.user.email + "'><br>");
+    $elem.append("<input type='text' value='" + $scope.user.github + "'><br>");
+    $elem.append("<input type='text' value='" + $scope.user.twitter + "'><br>");
+  }
+
 }]);
