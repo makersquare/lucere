f9643dac4623b82707b7d133848881ae478b1721
Tessa <tessakkelly@gmail.com>
2015-02-05 12:38:12 -0800

    add list of newly added users to admin create user page add remove button to remove users if mistake has been made

--- assets/js/controllers/user_creation_ctrl.js
+++ assets/js/controllers/user_creation_ctrl.js
@@ -1,8 +1,20 @@
 app.controller("userCreationCtrl", ["$scope", "User", function($scope, User) {
   $scope.newUser = {};
+  $scope.users = [];
+  
   $scope.createUser = function() {
     var newUserObj = new User.User({name: $scope.newUser.name , email: $scope.newUser.email, github: $scope.newUser.githubName});
     newUserObj.$save();
+    $scope.users.push(newUserObj);
     $scope.newUser = {};
+  };
+
+  $scope.remove = function(userId) {
+    User.User.delete({id: userId});
+    $scope.users = $scope.users.filter(function(user) {
+      if(user.id !== userId) {
+        return user;
+      }
+    });
   };
 }]);
