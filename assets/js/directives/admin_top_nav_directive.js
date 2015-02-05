db419813c7362e22a91c5214ae9c1b43810f3702
Connor Kojimoto <cokojimoto@gmail.com>
2015-02-05 10:42:41 -0800

    fix dropdown, fix library/team button from useraccount view, username/logout button is removed upon logout

--- assets/js/directives/admin_top_nav_directive.js
+++ assets/js/directives/admin_top_nav_directive.js
@@ -4,19 +4,37 @@ app.directive("adminTopNavDirective", ["$location", "$http", "$route", "AuthServ
     link: function(scope, attr, elem) {
       AuthService.currentUser(function(user) {
         scope.currentUser = user;
+        scope.userArr = user.administrating;
+        scope.libraryTag = "Core";
         var teams = user.administrating;
         var id = parseInt($route.current.params.libraryId) || parseInt($route.current.params.teamId);
-
+        if (scope.userArr.length > 1) {
+          scope.dropCheck = true;
+        } else {
+          scope.dropCheck = false;
+        }
         for (var i = 0; i < teams.length; i++) {
           if (id === 1) {
             scope.libraryTag = "Core";
-            scope.teamList = teams;
+            scope.userArr.shift();
           } else if(teams[i].id === id) {
             scope.libraryTag = teams[i].name;
-            teams.splice(i,1);
-            teams.push({id:1,name:"Core"});
-            scope.teamList = teams;
+            //loop through user array to remove the name that will be displayed on nav bar
+            for (var j = 0; j < scope.userArr.length; j++) {
+              if(teams[i].name === scope.userArr[j].name) {
+                scope.userArr.splice(j,1);
+              }
+            }
+            //first item in list should be core or core should be displayed on nav bar
+            if (scope.userArr[0].name !== "Core") {
+              scope.userArr.unshift({id:1,name:"Core"});
+            } 
           }
+        }
+
+        scope.getTeam = function() {
+          var id = $route.current.params.libraryId || $route.current.params.teamId || user.administrating[0].id;
+          $location.path("/admin/team/"+id);
         }
       });
 
@@ -26,24 +44,13 @@ app.directive("adminTopNavDirective", ["$location", "$http", "$route", "AuthServ
         $location.path("/login");
       };     
 
-      scope.getLibrary = function() {
-        var teamId = $route.current.params.teamId || $route.current.params.libraryId;
-        $location.path("/admin/library/"+teamId);
+      scope.changeLibrary = function(id) {
+          $location.path("/admin/team/"+id);        
       };
 
-      scope.getTeam = function() {
-        var libraryId = $route.current.params.libraryId || $route.current.params.teamId;
-        $location.path("/admin/team/"+libraryId);
-      }
-
-      scope.changeLibrary = function(id) {
-        var libraryId = $route.current.params.libraryId;
-        if (libraryId === 1) {
-          $location.path("/admin/library/"+id);
-        } else {
-          $location.path("/admin/team/"+id)
-        }
-        
+      scope.getLibrary = function() {
+        var id = $route.current.params.libraryId || $route.current.params.teamId || 1; //will need to checkout library in first administrating
+        $location.path("/admin/library/"+id);
       };
     }
   }
