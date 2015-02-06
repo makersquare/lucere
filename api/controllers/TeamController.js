a9269b3f4004075b6d7a2dd731ff5ad19d7d7542
Dudemullet <elpaip@gmail.com>
2015-02-05 17:54:58 -0800

    Herokue might be crashing because of this case sensitivity

--- api/controllers/TeamController.js
+++ api/controllers/TeamController.js
@@ -5,7 +5,7 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
-var Core = require("../services/CoreLibrary.js");
+var Core = require("../services/coreLibrary.js");
 
 module.exports = {
   create: function(req, res) {
