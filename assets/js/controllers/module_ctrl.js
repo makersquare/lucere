8be69561b882e10d002e0c8a2fb3984a92621201
Shehzan <shehzan@makersquare.com>
2015-02-07 02:57:29 -0800

    Remove admin module directive. Update side bar when adding lessons

--- assets/js/controllers/module_ctrl.js
+++ assets/js/controllers/module_ctrl.js
@@ -1,3 +1,23 @@
-app.controller("ModuleCtrl", ["$scope", "Module", "$routeParams", function($scope, Module, $routeParams) {
-  $scope.module = Module.get({id: $routeParams.moduleId});
+app.controller("ModuleCtrl", ["$scope", "Module", "$routeParams", "StateTracker", "Lesson", function($scope, Module, $routeParams, StateTracker, Lesson) {
+  var moduleId = $routeParams.moduleId;
+  $scope.module = Module.get({id: moduleId});
+
+  $scope.createLesson = function(title) {
+    var lesson = new Lesson({title: title});
+
+    lesson.$save(function(data) {
+      $scope.module.lessons.push(data.id);
+      $scope.module.$save()
+        .then(function() {
+          StateTracker.refreshLibrary();
+        });
+      $scope.newLessonTitle = "";
+    });
+  };
+
+  $scope.updateName = function() {
+    $scope.module.$update(function() {
+      StateTracker.refreshLibrary();
+    });
+  };
 }]);
