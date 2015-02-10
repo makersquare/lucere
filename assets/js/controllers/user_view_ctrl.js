0cfa8cc31e548762c1e7fb4c81798af17a9f8ade
Shehzan <shehzan@makersquare.com>
2015-02-10 10:40:15 -0800

    Remove user_view Ctrl

--- assets/js/controllers/user_view_ctrl.js
+++
@@ -1,3 +0,0 @@
-app.controller("UserViewCtrl", ["$scope", "User", "AuthService", function($scope, User, AuthService){
-  $scope.admin = AuthService.currentUser && (AuthService.currentUser.administrating.length !== 0);
-}]);
